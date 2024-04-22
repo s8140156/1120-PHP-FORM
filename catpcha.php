500af91cb153583bcb7e763ea4420e03d961cf4b
s8140156 <s8140156@gmail.com>
2024-04-22 17:17:24 +0800

    認識圖形驗證碼程式邏輯

--- catpcha.php
+++ catpcha.php
@@ -66,7 +66,7 @@ function captcha($str){
     $dst_w=0;
     $dst_h=0;
 
-    //使用迴圈來逐一分析每個字元的圖形資訊
+    //使用迴圈來逐一分析每個字元的圖形資訊(第一個迴圈)
     for($i=0;$i<mb_strlen($gstr);$i++){
 
         //使用mb_substr()順序取出每一個字元
@@ -89,7 +89,7 @@ function captcha($str){
         //比較每一次字元的高度來決定最大高度
         $dst_h=($dst_h>=$text_info[$char]['height'])?$dst_h:$text_info[$char]['height'];
 
-        //根據字型的資訊來取得字元的左上角坐標
+        //根據字型的資訊來取得字元的左上角坐標(算是每個字元圖片的起始點嗎)
         $text_info[$char]['x']=min($tmp[0],$tmp[2],$tmp[4],$tmp[6]);
         $text_info[$char]['y']=min($tmp[1],$tmp[3],$tmp[5],$tmp[7]);
     }
@@ -104,14 +104,14 @@ function captcha($str){
     //根據計算出來的驗證碼圖形完整寬高來建立一個全彩圖形資源
     $dst_img=imagecreatetruecolor($base_w,$base_h);
 
-    //顏色定義區
+    //顏色定義區(背景顏色)
     $white=imagecolorallocate($dst_img,255,255,255);
     $black=imagecolorallocate($dst_img,0,0,0);
     $blue=imagecolorallocate($dst_img,0,0,255);
     $red=imagecolorallocate($dst_img,255,0,0);
     $green=imagecolorallocate($dst_img,0,255,0);
 
-    //顏色陣列(在實務時,在選色時要考量使用者情形 如色盲)
+    //顏色陣列(在實務時,在選色時要考量使用者情形 如色盲(例如紅綠色的選用);線條/字型色彩避免與背景顏色相似)
     $colors=[imagecolorallocate($dst_img,255, 127, 80),
              imagecolorallocate($dst_img,204, 85, 0),
              imagecolorallocate($dst_img,184, 115, 51),
@@ -137,14 +137,14 @@ function captcha($str){
     //建立一個開始繪製文字圖形的起始坐標，由邊框的厚度開始繪製
     $x_pointer=$border;
 
-    //使用迴圈把驗證碼文字逐一寫入到圖片中
+    //使用迴圈把驗證碼文字逐一寫入到圖片中 (第二個迴圈)
     foreach($text_info as $char => $info){
         
         //計算放置的y坐標範圍，字元的高度加上邊框起始點(5)及總高度-底部坐標終點的限制(5)
         $y=rand($info['height']+5,$info['height']+($border*2-5*2));
 
         //將字元依照大小，角度，坐標，顏色，字型等資訊畫在畫布上
-        imagettftext($dst_img,$fontsize,$info['angle'],$x_pointer,$y,$colors[rand(0,count($colors)-1)],realpath('./fonts/arial.ttf'),$char);
+        imagettftext($dst_img,$fontsize,$info['angle'],$x_pointer,$y,$colors[rand(0,count($colors)-1)],realpath('./fonts/calibrib.ttf'),$char);
 
         //依照字元的寬度及字元的x坐標來產生下一個字元的x坐標起點
         $x_pointer=$x_pointer+$info['width']+$info['x']+1;
@@ -173,7 +173,8 @@ function captcha($str){
         imageline($dst_img,$left_x,$left_y,$right_x,$right_y,$colors[rand(0,count($colors)-1)]);
     }
     
-    //開啟輸出緩衝區(output buffer)
+    //開啟輸出緩衝區(output buffer) 
+    // 目的：由於圖形處理花時間，有可能串流資訊會來不及輸出到硬碟 先暫時開記憶體緩衝區 先把這些二進位資訊收下來 再看要輸出成什麼檔案類型(文字、圖形等)
     ob_start();
 
     //產生png格式的圖片，此時會先暫時存放在緩衝區中不送出去
@@ -185,8 +186,9 @@ function captcha($str){
     //刪除處理完畢的圖形資源資料
     imagedestroy($dst_img);
 
-    //使用base64的方式把緩衝區中的二進位圖形資料轉成base64的字串格式回傳出去
+    //使用base64的方式把緩衝區中的二進位圖形資料轉成base64的字串格式回傳出去(在畫面上使用檢視原始碼來看)
     //前方的data:image/png:base64, 是資料格式的宣告,讓瀏灠器可以知道這一段文字的功能是什麼=>可以讓瀏覽器可以打出圖型(但其實不是圖檔是字串)
+    // 如這類可能多人同時間使用登入驗證時 不要輕易存成檔案 改存成base64格式(轉成字串是存在客戶端 不是server端)分散loading
     return "data:image/png;base64," . base64_encode($output);
 
 }
